57e49e015b2181729826d38bce948181bc68c46b
Josh Mandel <jmandel@gmail.com>
2017-11-17 14:46:32 +0100

    Executables

--- 02-load.sh
+++ 02-load.sh
@@ -6,6 +6,7 @@ bq mk synthea
 
 for resource in $(gsutil ls gs://fhir-synthetic-ma/2017-05-24 | egrep -o '[A-Z][^.]+')
 do
+    echo "Load $resource"
     bq load --replace  --source_format=NEWLINE_DELIMITED_JSON   --autodetect \
-        synthea.${resource} gs://fhir-synthetic-ma/2017-05-24/${resource}.ndjson.gz
+        synthea.${resource} gs://fhir-synthetic-ma/2017-05-24/${resource}.ndjson.gz &
 done
